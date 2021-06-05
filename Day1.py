68b7bd028f682e43ef8edbba451655cd4ca7e9fd
Anand Gowda <32433566+Anandgowda18@users.noreply.github.com>
2021-06-05 23:31:45 +0530

    Day1 Python codes

---
+++ Day1.py
@@ -0,0 +1,24 @@
+#project
+print("welcome to the Band name generator")
+city_name = input("Enter the city you were born: ")
+pet_name = input("Enter your pet name: ")
+print(f"your band name could be {city_name} {pet_name}")
+
+#coding exercise(Print)
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print(\"what to print\")")
+
+#coding exercise(Print with new line)
+print('Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.\ne.g. print("Hello " + "world")\nNew lines can be created with a backslash and n.')
+
+#coding exercise with len,input and print function
+print(len(input("What is your name?\n")))
+
+#coding exercise for swapping numbers(variables)
+
+a = input("a: ")
+b = input("b: ")
+a,b = b,a
+print("a: ",a)
+print("b: ",b)
